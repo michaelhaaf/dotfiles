11874341dadf4f5bddaab8876ca3fa96a5fc6661
michaelhaaf <michael.haaf@gmail.com>
2022-10-08 16:05:37 -0400

    removed virtual text from nvim lsp (sorta, half working)

--- nvchad/.config/nvim/lua/custom/plugins/lspconfig.lua
+++ nvchad/.config/nvim/lua/custom/plugins/lspconfig.lua
@@ -1,8 +1,11 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
+local util = require 'lspconfig.util'
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "emmet_ls", "clangd", "jsonls", "tsserver" }
+local servers = util.available_servers()
+
+vim.diagnostic.config({ virtual_text = false })
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {

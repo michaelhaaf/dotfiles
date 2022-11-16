0a85d1ad4db6c49f64c1ee525dd302093e4cc8e1
michaelhaaf <michael.haaf@gmail.com>
2022-11-16 02:07:51 -0500

    Update list of lsps to ensure for installation
    Also, applied the reformatter to test that the lsp changes were working.
    Too lazy to split it into two commits

--- nvchad/.config/nvim/lua/custom/plugins/overrides.lua
+++ nvchad/.config/nvim/lua/custom/plugins/overrides.lua
@@ -3,101 +3,113 @@
 local M = {}
 
 M.treesitter = {
-  ensure_installed = {
-    "vim",
-    "html",
-    "css",
-    "javascript",
-    "json",
-    "toml",
-    "markdown",
-    "c",
-    "bash",
-    "lua",
-    "norg",
-    "python",
-  },
+	ensure_installed = {
+		"vim",
+		"html",
+		"css",
+		"javascript",
+		"json",
+		"toml",
+		"markdown",
+		"c",
+		"bash",
+		"lua",
+		"norg",
+		"python",
+	},
 }
 
 M.nvimtree = {
-  view = {
-    adaptive_size = false,
-    hide_root_folder = false,
-  },
+	view = {
+		adaptive_size = false,
+		hide_root_folder = false,
+	},
 
-  git = {
-    enable = true,
-  },
+	git = {
+		enable = true,
+	},
 
-  renderer = {
-    add_trailing = true,
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
-    },
-  },
+	renderer = {
+		add_trailing = true,
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
 }
 
 M.blankline = {
-  filetype_exclude = {
-    "help",
-    "terminal",
-    "alpha",
-    "packer",
-    "lspinfo",
-    "TelescopePrompt",
-    "TelescopeResults",
-    "nvchad_cheatsheet",
-    "lsp-installer",
-    "norg",
-    "",
-  },
+	filetype_exclude = {
+		"help",
+		"terminal",
+		"alpha",
+		"packer",
+		"lspinfo",
+		"TelescopePrompt",
+		"TelescopeResults",
+		"nvchad_cheatsheet",
+		"lsp-installer",
+		"norg",
+		"",
+	},
 }
 
 M.whichkey = {
-  plugins = {
-    spelling = {
-      enabled = true,
-    },
-  },
+	plugins = {
+		spelling = {
+			enabled = true,
+		},
+	},
 }
 
 M.alpha = {
-  header = {
-    val = {
-      "           ▄ ▄                   ",
-      "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
-      "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
-      "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
-      "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
-      "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄ ",
-      "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █ ",
-      "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █ ",
-      "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█     ",
-    },
-  },
+	header = {
+		val = {
+			"           ▄ ▄                   ",
+			"       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
+			"       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
+			"    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
+			"  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
+			"  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄ ",
+			"▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █ ",
+			"█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █ ",
+			"    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█     ",
+		},
+	},
 }
 
 M.mason = {
-  ensure_installed = {
-    -- lua stuff
-    "lua-language-server",
-    "stylua",
+	ensure_installed = {
+		-- lua stuff
+		"lua-language-server",
+		"stylua",
 
-    -- web dev
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "deno",
-    "emmet-ls",
-    "json-lsp",
+		-- web dev
+		"css-lsp",
+		"html-lsp",
+		"typescript-language-server",
+		"deno",
+		"emmet-ls",
+		"json-lsp",
+		"prettier",
+		"xo",
 
-    -- shell
-    "shfmt",
-    "shellcheck",
-  },
+		-- shell
+		"shfmt",
+		"shellcheck",
+		"beautysh",
+
+		-- python/c
+		"flake8",
+		"autopep8",
+		"clang-format",
+		"rustfmt",
+
+		-- documentation
+		"proselint",
+	},
 }
 
 return M

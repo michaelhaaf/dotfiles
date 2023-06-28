932ca35cbd8e9398d318c11a54351e37610389a6
michaelhaaf <michael.haaf@gmail.com>
2023-06-28 10:56:01 -0400

    move from nvchad to astrovim

---
+++ astrovim/.config/nvim/lua/user/plugins/community.lua
@@ -0,0 +1,10 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of imporing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+  { import = "astrocommunity.colorscheme.rose-pine",  enabled = true },
+  { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+}

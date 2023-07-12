64323dd47d0f8948cef4c38f236fafc3f80bb311
michaelhaaf <michael.haaf@gmail.com>
2023-07-12 14:39:27 -0400

    theme and lint/formatting tweaks to astrovim

--- astrovim/.config/nvim/lua/user/init.lua
+++ astrovim/.config/nvim/lua/user/init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "catppuccin",
+  colorscheme = "rose-pine-moon",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- astrovim/.config/nvim/lua/user/plugins/null-ls.lua
+++ astrovim/.config/nvim/lua/user/plugins/null-ls.lua
@@ -9,8 +9,23 @@ return {
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
       -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettierd,
+      null_ls.builtins.formatting.autopep8,
+      null_ls.builtins.formatting.beautysh,
+      null_ls.builtins.code_actions.refactoring,
+      null_ls.builtins.code_actions.shellcheck,
+      null_ls.builtins.code_actions.eslint_d,
+      null_ls.builtins.code_actions.gitsigns,
+      null_ls.builtins.diagnostics.actionlint,
+      null_ls.builtins.diagnostics.checkmake,
+      null_ls.builtins.diagnostics.flake8,
+      null_ls.builtins.diagnostics.ktlint,
+      null_ls.builtins.diagnostics.markdownlint_cli2,
+      null_ls.builtins.diagnostics.shellcheck,
+      null_ls.builtins.diagnostics.stylelint,
+      null_ls.builtins.diagnostics.typos,
+      null_ls.builtins.completion.luasnip,
     }
     return config -- return final config table
   end,
